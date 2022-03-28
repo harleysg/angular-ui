0aab02cb838ca4cb13883915714e123183ffdfc7
harley santos garzon <harley.santos.garzon@gmail.com>
2022-03-28 11:39:26 -0500

    refactor(sg-angular-library): add gallery module

---
+++ projects/ui-angular-library/src/lib/ui-angular-library.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { GridGalleryModule } from './sg-ui-grid-gallery/sg-ui-grid-gallery.module';
+
+@NgModule({
+  declarations: [],
+  imports: [GridGalleryModule],
+  exports: [],
+})
+export class UiAngularLibraryModule {}
