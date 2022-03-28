d08bb5c02f488025343c7c7cd3940df247600cfe
harley santos garzon <harley.santos.garzon@gmail.com>
2022-03-28 11:39:51 -0500

    refactor(public-api): update grid-gallery

---
+++ projects/ui-angular-library/src/public-api.ts
@@ -0,0 +1,13 @@
+/*
+ * Public API Surface of ui-angular-library
+ */
+
+export * from './lib/ui-angular-library.module';
+// grid-gallery
+export * from './lib/sg-ui-grid-gallery/sg-ui-grid-gallery.module';
+export * from './lib/sg-ui-grid-gallery/sg-ui-grid-gallery.component';
+export * from './lib/sg-ui-grid-gallery/constants/index';
+export * from './lib/sg-ui-grid-gallery/interfaces/picsum.interface';
+export * from './lib/sg-ui-grid-gallery/interfaces/image.interface';
+export * from './lib/sg-ui-grid-gallery/services/hooks.service';
+export * from './lib/sg-ui-grid-gallery/services/gallery.service';
