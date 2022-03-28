ec9b12215bed6002c1043a96a365c11b5354fdee
harley santos garzon <harley.santos.garzon@gmail.com>
2022-03-28 11:37:21 -0500

    feat(sg-ui-grid-gallery): add constants

---
+++ projects/ui-angular-library/src/lib/sg-ui-grid-gallery/constants/index.ts
@@ -0,0 +1,27 @@
+const ASPECT_RATIO = {
+  '3_2': {
+    val: '3 / 2',
+    h: 2 / 3,
+    w: 3 / 2
+  }
+}
+
+export const CSS_CLASSLIST = {
+  oneColumn: 'has-one-column',
+  showTriad: 'by-triad'
+}
+
+const GALLERY_LIMIT_GROUP = 3
+
+const IMAGE_SIZE = {
+  big: {
+    w: 840,
+    h: 840 * ASPECT_RATIO['3_2'].h
+  },
+  small: {
+    w: 277,
+    h: 277 * ASPECT_RATIO['3_2'].w
+  }
+}
+
+export { ASPECT_RATIO, IMAGE_SIZE, GALLERY_LIMIT_GROUP }
