6c003b5c017f22e77766fd2ec1e55fb5eb4791e3
harley santos garzon <harley.santos.garzon@gmail.com>
2022-03-28 22:24:08 -0500

    refactor(sg-ui-gallery): clean component

--- projects/ui-angular-library/src/lib/sg-ui-grid-gallery/sg-ui-grid-gallery.component.ts
+++ projects/ui-angular-library/src/lib/sg-ui-grid-gallery/sg-ui-grid-gallery.component.ts
@@ -4,58 +4,40 @@ import {
   ElementRef,
   Input,
   OnInit,
-  ViewChild,
-} from '@angular/core';
-import { GalleryService } from './services/gallery.service';
-import { GroupGallery } from './interfaces/picsum.interface';
-import { IGallery, GalleryByStateroom } from './interfaces/picsum.interface';
-import { IMAGE_SIZE, GALLERY_LIMIT_GROUP } from './constants/index';
+  ViewChild
+} from '@angular/core'
+import { GalleryService } from './services/gallery.service'
+import { IGallery, GalleryByStateroom } from './interfaces/picsum.interface'
+import { IMAGE_SIZE, GALLERY_LIMIT_GROUP } from './constants/index'
 
 @Component({
   selector: 'sg-ui-grid-gallery',
   templateUrl: './index.html',
-  styleUrls: ['./index.scss'],
+  styleUrls: ['./index.scss']
 })
 export class GridGalleryComponent implements OnInit, AfterViewInit {
-  @Input() showMesagge: boolean = false;
-  @Input() galleryData!: GalleryByStateroom;
-  @Input() galleryName!: string;
+  @Input() showMesagge: boolean = false
+  @Input() galleryData!: GalleryByStateroom
+  @Input() galleryName!: string
 
-  @ViewChild('galleryRef', { static: false }) galleryRef!: ElementRef;
+  @ViewChild('galleryRef', { static: false }) galleryRef!: ElementRef
 
-  LIMIT = GALLERY_LIMIT_GROUP;
-  gallery!: IGallery;
-  message?: any; // ---- Optional
+  LIMIT = GALLERY_LIMIT_GROUP
+  gallery!: IGallery
 
   constructor(private galleryService: GalleryService) {}
 
   ngOnInit() {
-    this.getGallery();
+    this.getGallery()
   }
 
   ngAfterViewInit() {
     if (this.galleryRef) {
-      this.galleryRef.nativeElement.style.maxWidth = `${IMAGE_SIZE.big.w}px`;
+      this.galleryRef.nativeElement.style.maxWidth = `${IMAGE_SIZE.big.w}px`
     }
   }
 
   getGallery() {
-    this.gallery = this.galleryService.buildGallery(this.galleryData.list);
-
-    if (this.showMesagge)
-      this.setMessage({
-        main: this.gallery.main.list,
-        last: this.gallery.last.list,
-      }); // ---- Optional
-  }
-
-  // ------------------------------------------------- Optional
-  setMessage({ main, last }: GroupGallery) {
-    this.message = {
-      gallery: main.length * 3,
-      lastGallery: last.length,
-      isLastGallery: this.gallery.last.show,
-      resetLayout: main.length === 1 && main[0]['length'] === this.LIMIT - 1,
-    };
+    this.gallery = this.galleryService.buildGallery(this.galleryData.list)
   }
 }
